0aa71c0c68758d667079411c4aef6b6b69afe382
VIREAK KEOSOKVIBOL <46261326+vireakkeosokvibol@users.noreply.github.com>
2021-07-16 15:44:08 +0700

    Add support for fastify and graphql

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [],
+  imports: [GraphQLModule.forRoot({
+    autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+  }), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/user/user.resolver.ts
@@ -0,0 +1,10 @@
+import { Query, Resolver } from '@nestjs/graphql';
+import { UserModel } from './user.model';
+
+@Resolver()
+export class UserResolver {
+  @Query(returns => UserModel)
+  async user(): Promise<UserModel> {
+    return { id: 'a', email: 'test' }
+  }
+}
